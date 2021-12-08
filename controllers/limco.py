d60f586939b559560f5a7e9faf65f800dd982b9c
mfieldsgrp1 <94088124+mfieldsgrp1@users.noreply.github.com>
2021-12-07 19:59:56 -0500

    update
    contacts

--- controllers/limco.py
+++ controllers/limco.py
@@ -13,14 +13,15 @@ def importp():
             home_city= line[3]
             states= line[4]
             home_zip=line[5]
-            company = line[6]
+            company_id = line[6]
             title = line[7]
             office_phone = line[8]
             cell_phone = line[9]
             email=line[10]
             contact_type=line[11]
             company_id=db.companies.update_or_insert(company_name=name)
-            db.contacts.update_or_insert(company_id=company_id, name=name, home_address=home_address, home_city=home_city,states=states,home_zip=postal_code)
+            db.contacts.update_or_insert(company_id=company_id, name=name, home_address=home_address, home_city=home_city,states=states,home_zip=zip_code, title=title,
+            office_phone=office_phone, cell_phone=cell_phone, email=email, contact_type=contact_type)
             lines += 1
         session.lines = lines
         response.flash = str(lines) + " lines read"
